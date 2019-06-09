0377e2082b88171b18c7136300e08e995a677db6
Jordan Roberts <jordanroberts@talk21.com>
2019-06-09 14:55:53 +0100

    Game continues until user types exit

--- lib/echo.rb
+++ lib/echo.rb
@@ -2,10 +2,18 @@ require 'Date'
 
 class Echo
 
+  attr_reader :phrase
+
   def echo
     puts "Say something"
-    phrase = gets.chomp!
-    return Date.today.strftime('%m/%d/%Y') + ' | ' + Time.now.strftime('%H:%M') + ' | ''You said: ' + phrase
+    @phrase = gets.chomp!
+    return Date.today.strftime('%m/%d/%Y') + ' | ' + Time.now.strftime('%H:%M') + ' | ''You said: ' + @phrase
   end
 
+  def start
+    until @phrase == "exit"
+      echo
+    end
+    "Goodbye!"
+  end
 end
