5d5781ff27255a8317ec4b71207232336fd76136
Jordan Roberts <jordanroberts@talk21.com>
2019-06-09 14:53:09 +0100

    added time to user output as per requirements

--- lib/echo.rb
+++ lib/echo.rb
@@ -5,7 +5,7 @@ class Echo
   def echo
     puts "Say something"
     phrase = gets.chomp!
-    return Date.today.strftime('%m/%d/%Y') + ' | ' + 'You said: ' + phrase
+    return Date.today.strftime('%m/%d/%Y') + ' | ' + Time.now.strftime('%H:%M') + ' | ''You said: ' + phrase
   end
 
 end
--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -7,7 +7,7 @@ let(:echo) { described_class.new }
   describe '#echo' do
     it 'repeats the user input' do
       allow(Date).to receive(:today).and_return Date.new(2019,6,10)
-      expect(echo.echo).to eq (Date.today.strftime('%m/%d/%Y') + ' | ' + "You said: hello")
+      expect(echo.echo).to eq (Date.today.strftime('%m/%d/%Y') + ' | ' + Time.now.strftime('%H:%M') + ' | ' + "You said: hello")
     end
   end
 end
