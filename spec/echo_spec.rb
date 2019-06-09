656e0441ce44fb38cc71a40fd8fbd7fd648a8700
Jordan Roberts <jordanroberts@talk21.com>
2019-06-09 14:53:29 +0100

    Mocked time in tests

--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -7,6 +7,7 @@ let(:echo) { described_class.new }
   describe '#echo' do
     it 'repeats the user input' do
       allow(Date).to receive(:today).and_return Date.new(2019,6,10)
+      allow(Time).to receive(:now).and_return Time.new(12)
       expect(echo.echo).to eq (Date.today.strftime('%m/%d/%Y') + ' | ' + Time.now.strftime('%H:%M') + ' | ' + "You said: hello")
     end
   end
